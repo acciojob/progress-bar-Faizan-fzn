972dd14d7adc6ac6c46b0b1a845999e1fc4fc175
Faizan Ahmed <153298469+Faizan-fzn@users.noreply.github.com>
2024-02-29 02:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// //your JS code here. If required.
+
+// let prev = document.getElementById("prev");
+// let next = document.getElementById("next");
+
+// prev.addEventListener("click", backward);
+// next.addEventListener("click", forward);
+
+// const connection = document.getElementsByClassName("connection");
+// const circle = document.getElementsByClassName("circle");
+
+// let i = 0;
+// function forward(){
+//       connection[i].classList.add("active");
+// 	  circle[i+1].classList.add("active");
+// 	  i++;
+// 	prev.disabled = false;
+// }
+
+// // let i = 5
+// function backward(){
+	
+// 	circle[i].classList.remove("active");
+// 	connection[i-1].classList.remove("active");
+// 	  i--;
+// 	next.disabled = true;
+// }
+
 //your JS code here. If required.
 
 let prev = document.getElementById("prev");
@@ -15,14 +43,20 @@ function forward(){
 	  circle[i+1].classList.add("active");
 	  i++;
 	prev.disabled = false;
+	if(i === 4){
+		next.disabled = true;
+		i--;
+	}
 }
 
-// let i = 5
+
 function backward(){
-	
-	circle[i].classList.remove("active");
-	connection[i-1].classList.remove("active");
+	  connection[i].classList.remove("active");
+	  circle[i+1].classList.remove("active");
 	  i--;
-	next.disabled = true;
+	next.disabled = false;
+	if(i === -1){
+		prev.disabled = true;
+		i++;
+	}
 }
-
